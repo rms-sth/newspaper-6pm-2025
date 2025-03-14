58e3457410e1b61807e51a1ae2bdc582f08aa8a6
Ramesh Pradhan <pyrameshpradhan@gmail.com>
2025-03-14 18:29:52 +0545

    Add feature posts

--- NEWS/settings.py
+++ NEWS/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "NEWS.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ['templates'],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,9 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = ['static']
+STATICFILES_DIRS = ["static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "media"
--- newspaper/views.py
+++ newspaper/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from newspaper.models import Post
 
+from django.utils import timezone
+from datetime import timedelta
+
 
 class HomeView(ListView):
     model = Post
@@ -10,3 +13,26 @@ class HomeView(ListView):
     queryset = Post.objects.filter(
         published_at__isnull=False, status="active"
     ).order_by("-published_at")[:5]
+
+    # for sharing extra data to the template
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["featured_post"] = (
+            Post.objects.filter(published_at__isnull=False, status="active")
+            .order_by("-published_at", "-views_count")
+            .first()
+        )
+        context["featured_posts"] = Post.objects.filter(
+            published_at__isnull=False, status="active"
+        ).order_by("-published_at", "-views_count")[1:4]
+
+        one_week_ago = timezone.now() - timedelta(days=7)
+        context["weekly_top_posts"] = Post.objects.filter(
+            published_at__isnull=False, status="active", published_at__gte=one_week_ago
+        ).order_by("-published_at", "-views_count")[:7]
+
+        context["recent_posts"] = Post.objects.filter(
+            published_at__isnull=False, status="active"
+        ).order_by("-published_at")[:7]
+
+        return context
